371a6c3e15eeb65ba92eaff792871aed75d23608
melvin <tan.melvin@outlook.sg>
2026-04-27 09:51:53 +0800

    Initial commit from Spring Initializr

---
+++ src/main/java/com/talend/framework/metadata_framework/MetadataFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.talend.framework.metadata_framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetadataFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetadataFrameworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/talend/framework/metadata_framework/MetadataFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talend.framework.metadata_framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetadataFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
